ef05408e5701dbd1ff49f135ca3b5a45ea1a8612
kirillkeril <89710374+kirillkeril@users.noreply.github.com>
2023-06-08 14:14:50 +0500

    try to build vercel

--- app.js
+++ app.js
@@ -6,26 +6,32 @@ const hbs = require('hbs');
 const PORT = process.env.PORT || 80;
 
 app = express();
-app.engine(
-    'hbs',
-    expressHbs.engine({
-        layoutsDir: 'views/layouts',
-        defaultLayout: 'layout',
-        extname: 'hbs'
-    })
-);
-app.set('view engine', 'hbs');
 app.use(express.static('static'));
 
-hbs.registerPartials(path.resolve(__dirname + '/views/partials'));
-
-app.use('/some-lesson', (req,res) => {
-    res.render('someLesson')
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+})
+// app.engine(
+//     'hbs',
+//     expressHbs.engine({
+//         layoutsDir: 'views/layouts',
+//         defaultLayout: 'layout',
+//         extname: 'hbs'
+//     })
+// );
+// app.set('view engine', 'hbs');
+//
+// hbs.registerPartials(path.resolve(__dirname + '/views/partials'));
+//
+// app.use('/some-lesson', (req,res) => {
+//     res.render('someLesson')
+// });
+// app.use('/', (req, res) => {
+//     res.render('index');
+// });
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
-app.use('/', (req, res) => {
-    res.render('index');
-});
-// app.listen(PORT);
 
 
 module.exports = app
