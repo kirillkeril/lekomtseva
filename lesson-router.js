47a48db84cda52de6b52f8238ba8ba0ee06aaddb
kirillkeril <89710374+kirillkeril@users.noreply.github.com>
2023-06-19 23:37:48 +0500

    add lesson 2

--- lesson-router.js
+++ lesson-router.js
@@ -32,5 +32,17 @@ router.get('/t1l1test', (req, res) => {
 router.get('/t1l2s1', (req, res) => {
     res.render('t1l2s1');
 })
+router.get('/t1l2s2', (req, res) => {
+    res.render('t1l2s2');
+})
+router.get('/t1l2s3', (req, res) => {
+    res.render('t1l2s3');
+})
+router.get('/t1l2s4', (req, res) => {
+    res.render('t1l2s4');
+})
+router.get('/t1l2s5', (req, res) => {
+    res.render('t1l2s5');
+})
 
 module.exports = router;
